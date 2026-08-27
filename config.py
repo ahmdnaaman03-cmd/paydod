1b44571245fd80a5de1fbd3b5eb25817f4e332a1
ahmdnaaman03-cmd <Ahmdnaaman03-cmd@users.noreply.github.com>
2026-08-27 16:45:45 +0000

    Fix Config class import

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///paydod.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-APP_BASE_URL = 'https://Ahmdnoaman.pythonanywhere.com'
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///paydod.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APP_BASE_URL = 'https://Ahmdnoaman.pythonanywhere.com'
